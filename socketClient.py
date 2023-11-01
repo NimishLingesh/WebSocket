01e81bd10c5e1b3d4134b75c35b1999d929e60d2
Nimish Lingesh <111670449+NimishLingesh@users.noreply.github.com>
2023-10-31 20:00:11 -0700

    init commit

---
+++ socketClient.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import asyncio
+from random import randint
+from websockets.server import serve
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send('[' + str(randint(1, 10)) + '] ' + message)
+
+async def main():
+    async with serve(echo, "localhost", 8765):
+        await asyncio.Future() # run forever
+        
+asyncio.run(main())
